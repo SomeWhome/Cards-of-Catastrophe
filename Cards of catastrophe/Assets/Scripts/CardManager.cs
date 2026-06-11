5015e8a2ceb34f27292e16a057d1970c078ed6ba
SomeWhome <oliverhopkins2009@gmail.com>
2026-06-11 14:08:45 +0100

    More scripts added

---
+++ Cards of catastrophe/Assets/Scripts/CardManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public GameObject SnapSpot1;
+    public bool SnapActive1;
+    public bool SnapSpotTaken1;
+    public GameObject SnapSpot2;
+    public bool SnapActive2;
+    public bool SnapSpotTaken2;
+    public GameObject SnapSpot3;
+    public bool SnapActive3;
+    public bool SnapSpotTaken3;
+    public GameObject SnapSpot4;
+    public bool SnapActive4;
+    public bool SnapSpotTaken4;
+    public GameObject SnapSpot5;
+    public bool SnapActive5;
+    public bool SnapSpotTaken5;
+
+}
