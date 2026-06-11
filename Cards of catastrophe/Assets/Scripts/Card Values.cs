1ddac15a6c5bb56707864c809ffb98f5a8f3f563
SomeWhome <oliverhopkins2009@gmail.com>
2026-06-11 12:08:19 +0100

    Card values

---
+++ Cards of catastrophe/Assets/Scripts/Card Values.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CardValues : MonoBehaviour
+{
+    public int cardValue;
+    public int CardValueChange;
+
+    public void CardValuesChange(int CardValueChange, int cardValue)
+    {
+        cardValue += CardValueChange;
+    }
+}
