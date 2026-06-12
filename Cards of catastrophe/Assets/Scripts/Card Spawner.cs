0f6b5f691cf3efc82b099f57fc5c222178535762
SomeWhome <oliverhopkins2009@gmail.com>
2026-06-12 12:13:47 +0100

    Random Card Spawner

---
+++ Cards of catastrophe/Assets/Scripts/Card Spawner.cs
@@ -0,0 +1,63 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+public class CardSpawner : MonoBehaviour
+{
+
+    public GameObject[] Cards;
+    public bool[] Delt;
+    public bool PlayerGo = true;
+    private int CollectiveScore = 0;
+    private bool AllCardPicked = true;
+    private bool delt;
+
+    public int AmmountofCardsSpawned = 0;  
+    public Vector3 spawnPosition = Vector3.zero;
+    public Vector3 positionRange = new Vector3(5f, 0f, 5f);
+    public void Hit()
+    {
+        if (PlayerGo && CollectiveScore <= 21)
+        {
+            if (!AllCardPicked)
+            {
+                SpawnRandomCard();
+            }
+        }
+    }
+
+    public void SpawnRandomCard()
+    {
+        if (Cards == null || Cards.Length == 0)
+        {
+            Debug.LogError("No prefabs assighned in the inspector!");
+            return;
+        }
+
+        if (AmmountofCardsSpawned == 11)
+        {
+            int randomIndex = Random.Range(0, Cards.Length);
+            if (Delt[randomIndex])
+            {
+                while (Delt[randomIndex])
+                {
+                    int randomIndex1 = Random.Range(0, Cards.Length);
+                    randomIndex = randomIndex1;
+                }
+                Delt[randomIndex] = true;
+            }
+            else
+            {
+                Delt[randomIndex] = true;
+            }
+
+
+            if (Cards[randomIndex] == null)
+            {
+                Debug.LogError($"prefab at index {randomIndex} is null");
+                return;
+            }
+            Vector3 finalposition = spawnPosition;
+            Instantiate(Cards[randomIndex], finalposition, Quaternion.identity);
+            AmmountofCardsSpawned += 1;
+        }
+    }
+}
